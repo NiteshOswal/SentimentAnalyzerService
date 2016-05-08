9af063db9ef8b6c4c2ce3979807cc1533675f67e
Nitesh Oswal <nit.oswal@gmail.com>
2016-05-08 14:24:47 +0530

    Web UI changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const path = require('path'),
+    express = require('express'),
+    async = require('async'),
+    cp = require('child_process'),
+    router = express.Router(),
+    lib = require('../lib');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api', (req, res) => {
+    /**
+     * Gets data from req.query, 2 important keys here
+     * topic - The topic to pull for
+     * count - The number of tweets to process
+     */
+    console.log(JSON.stringify({topic: req.query.topic, count: req.query.count}));
+    const worker = cp.fork(path.join(__dirname, "../lib/worker"), [JSON.stringify({topic: req.query.topic, count: req.query.count})]);
+
+    worker.on("message", (m) => {
+        if(m == "exit") {
+            worker.send("exit");
+        }
+    });
+
+    res.send({status: true});
+});
+
+router.get('/:slug', function(req, res) {
+
 });
 
 module.exports = router;
