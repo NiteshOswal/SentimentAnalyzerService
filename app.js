57d2cba3434fe9a1d4fd8dd9f9836df1bf1c19fc
Nitesh Oswal <nit.oswal@gmail.com>
2016-05-08 20:11:39 +0530

    Update app.js, create new middleware for maxmind & IP stuff

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+'use strict';
+
 const express = require('express'),
     path = require('path'),
     favicon = require('serve-favicon'),
     logger = require('morgan'),
+    maxmind = require('maxmind'),
+    ip = require('ip'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser');
+
+maxmind.init([path.join(__dirname, "/data/GeoLiteCity.dat"), path.join(__dirname, "/data/GeoLiteCityv6.dat")]);
 
 const routes = require('./routes/index');
 const publicDirs = ['public', 'bower_components'];
@@ -19,13 +25,28 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  req['sas'] = {};
+  let ip_address = "116.203.72.25", //ip.address(),
+    location = {};
+  if(ip.isV4Format(ip_address)) {
+    location = maxmind.getLocation(ip_address) || location;
+  } else if(ip.isV6Format(ip_address)) {
+    location = maxmind.getLocationV6(ip_address) || location;
+  }
+  req.sas.ip = ip_address;
+  req.sas.location = location;
+  next();
+});
+
 publicDirs.forEach((d) => { app.use("/" + d, express.static(path.join(__dirname, d))); });
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
