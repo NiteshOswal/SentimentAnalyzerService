5a72b16690a49d471a9f3fb7e504c8efb9fb69b1
Nitesh Oswal <nit.oswal@gmail.com>
2016-05-09 00:23:08 +0530

    Start saving ratings upon request on the Web Interface

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@
 const path = require('path'),
     express = require('express'),
     async = require('async'),
+    mongoose = require('mongoose'),
     base62 = require('base62'),
     cp = require('child_process'),
     router = express.Router(),
-    lib = require('../lib');
+    lib = require('../lib'),
+    logger = lib.helpers.logger;
+
+require('../lib/models/ratings.model')();
+require('../lib/models/views.model')();
+
+const Ratings = mongoose.model('Ratings'),
+    Views = mongoose.model('Views');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -19,24 +27,69 @@ router.get('/api', (req, res) => {
      * topic - The topic to pull for
      * count - The number of tweets to process
      */
-    const slug = base62.encode(req.query.topic),
-        worker = cp.fork(path.join(__dirname, "../lib/worker"), [JSON.stringify({topic: req.query.topic, count: req.query.count})]);
-    
+    const worker = cp.fork(path.join(__dirname, "../lib/worker"), [JSON.stringify({topic: req.query.topic, count: req.query.count})]);
+    let slug = "";
     worker.on("message", (m) => {
         if(m === "exit") {
-            res.end();
             worker.send("exit");
         } else {
             let temp = JSON.parse(m); //basically when we get back rating from the place
-            res.json(Object.assign({}, {status: true, location: req.sas, slug: slug}, temp));
+            Ratings.create(
+            {
+                negative_score: temp.negative_score,
+                positive_score: temp.positive_score,
+                total_lines: temp.total_lines,
+                total_score: temp.total_score,
+                slug: slug,
+                topic: req.query.topic,
+                count: req.query.count
+            },
+            {
+                upsert: true,
+                new: true,
+                setDefaultsOnInsert: true
+            }, function(err, data) {
+                if(err) {
+                    logger.error("Ratings Save Error ", err);
+                    return res.json({status: false, location: req.sas});
+                } else {
+                    logger.info("Ratings Saved ", data, "Finished");
+                    slug = base62.encode(parseInt(data._id));
+                    Ratings.update({_id: data._id}, {slug: slug}, function(err, data) {
+                        if(err) {
+                            logger.error("Slug Update Error ", err);
+                        } else {
+                            logger.info("Slug Update Success ", data);
+                        }
+                    });
+                    return res.json(Object.assign({}, {status: true, location: req.sas, slug: slug}, temp));
+                }
+            });
         }
     });
 });
 
 router.get('/:slug', (req, res) => {
-    const slug = base62.decode(req.params.slug);
-    console.log(req.sas);
-    res.json(req.sas);
+    Ratings.aggregate([
+        {$match: {slug: req.params.slug}},
+        {$sort: {created: -1}},
+        {$limit: 1}
+    ], function(err, data) {
+        if(err) {
+            logger.error("Get Slug ", req.params.slug, err);
+            return res.render('error', {
+              message: "So something happened",
+              error: err
+            });
+        } else {
+            if(data.length == 0) {
+                return res.json({status: false});
+            }
+            logger.info("Get Slug ", req.params.slug, data);
+            return res.json(Object.assign({status: true}, data[0]));
+        }
+    });
+
 });
 
 module.exports = router;
