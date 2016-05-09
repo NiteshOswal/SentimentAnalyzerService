5b433f8eefb530d4e9ac216dc0a3e2f2a88cfc7d
Nitesh Oswal <nit.oswal@gmail.com>
2016-05-09 08:49:55 +0530

    Changes to support the new UI for charts

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,49 @@
+/**
+ * Object.values polyfill
+ */
+
+const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+const concat = Function.bind.call(Function.call, Array.prototype.concat);
+const keys = Reflect.ownKeys;
+
+if (!Object.values) {
+  Object.values = function values(O) {
+    return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+  };
+}
+
 $(document).ready(function(){
-  $("#search").submit(function(event){
+  $("#search").submit(function(event) {
+    $('#details').show();
     var url = "/api?topic="+encodeURI($("#topic").val())+"&count="+$("#count").val();
     $('#htopic').html($("#topic").val());
     $('#hstatus').html("Processing...");
     $('#hrating').html("");
     $('#stars').html("");
     $.get(url, function(data){
-      var rating = data.percentage_score.toFixed(1);
-      var html = "";
-      var full_star = "<i class='fa fa-star'></i>";
-      var half_star = "<i class='fa fa-star-half-o'></i>";
-      var empty_star = "<i class='fa fa-star-o'></i>";
+      var rating = data.total_score.toFixed(1),
+        html = "",
+        full_star = "<i class='fa fa-star'></i>",
+        half_star = "<i class='fa fa-star-half-o'></i>",
+        empty_star = "<i class='fa fa-star-o'></i>";
+
+      var stats = new Chart($('#stats'), {
+          type: 'bar',
+          data: {
+              labels: Object.keys(data.score_by_date),
+              datasets: [
+                {
+                  label: "Score",
+                  data: Object.values(data.score_by_date).map(function(v) { return v.total_score; })
+                }
+              ]
+          }
+      });
 
       var t = rating*10/2;
       for(var i = 0; i < 5; i++) {
-        if(t >= 10){
+        if(t >= 10) {
           html += full_star;
         } else {
           if(t>5) {
